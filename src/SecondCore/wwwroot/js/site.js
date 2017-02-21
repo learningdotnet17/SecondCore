709be9a02570cdc7a9307ec44d2f670aa300dcab
brije <brije@DESKTOP-2SNJQRJ>
2017-02-20 21:57:31 -0500

    Add project files.

---
+++ src/SecondCore/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿//site.js
+
+(function () {
+
+    var ele = $("#username");
+    ele.text("Tanya Chawla");
+
+    var main = $("#main");
+    main.on("mouseenter", function () {
+        main.style = 'background-color: #800;';
+    });
+    main.on("mouseleave", function () {
+        main.style = "";
+    });
+
+    //var menuitems = $("ul.menu li a");
+    //menuitems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+
+})();
